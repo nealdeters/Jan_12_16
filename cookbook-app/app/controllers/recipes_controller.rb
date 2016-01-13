ac1e2f49b596250dcb08f90b7ab9c6b66475d2c6
nealdeters <nealdeters@gmail.com>
2016-01-12 20:22:03 -0600

    created cookbook app

---
+++ cookbook-app/app/controllers/recipes_controller.rb
@@ -0,0 +1,9 @@
+class RecipesController < ApplicationController
+  def meal
+    @recipe = Recipe.last
+  end
+
+  def every_meal
+    @recipes = Recipe.all
+  end
+end
---
+++ cookbook-app/config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  get '/' => 'recipes#meal'
+  get '/all' => 'recipes#every_meal'
+end
