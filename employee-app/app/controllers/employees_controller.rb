f8fce5cfdbdbda9115a4e8b0e8dd04a43e1ecb58
nealdeters <nealdeters@gmail.com>
2016-01-14 21:25:58 -0600

    updated employee app

--- employee-app/app/controllers/employees_controller.rb
+++ employee-app/app/controllers/employees_controller.rb
@@ -6,4 +6,18 @@ class EmployeesController < ApplicationController
   def all
     @employees = Employee.all
   end
+
+  def new
+    
+  end
+
+  def added
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @email = params[:email]
+    @phone = params[:phone]
+    @job_title = params[:job_title]
+    @salary = params[:salary]
+    @gender = params[:gender]
+  end
 end
--- employee-app/config/routes.rb
+++ employee-app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'employees#home'
   get '/all' => 'employees#all'
+  get '/new_employee' => 'employees#new'
+  post '/added_employee' => 'employees#added'
 end
