9b01675d39de9eb61658667b3dffc5c2f5902160
nealdeters <nealdeters@gmail.com>
2016-01-18 20:02:29 -0600

    added flash messaging to both apps

--- cookbook-app/app/controllers/recipes_controller.rb
+++ cookbook-app/app/controllers/recipes_controller.rb
@@ -4,4 +4,49 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def new
+    
+  end
+
+  def create
+    @recipe = Recipe.create({title: params[:title],
+                             chef: params[:chef],
+                             ingredients: params[:ingredients],
+                             directions: params[:directions]})
+    
+    flash[:success] = "New Recipe Created"
+
+    redirect_to "/"
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.update({title: params[:title],
+                    chef: params[:chef],
+                    ingredients: params[:ingredients],
+                    directions: params[:directions]})
+
+    flash[:info] = "Recipe Updated"
+
+    redirect_to "/recipes/#{@recipe.id}"
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    flash[:danger] = "Recipe Deleted"
+
+    redirect_to '/'
+  end
+
 end
